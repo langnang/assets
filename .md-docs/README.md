6bb8e5126ce0b427635debd4f774a6394ffcfa6c
langnang <langnang.chen@outlook.com>
2024-05-09 14:04:38 +0800

    format

--- .md-docs/README.md
+++ .md-docs/README.md
@@ -3,7 +3,7 @@
 > 存储
 
 - Applications - 应用
-- Awesome
+- Awesome - 资源列表
 - CheatSheet - 速查表
 - Softwares - 程序
 - Websites - 网站
--- 000.old/blog.langnang/usr/themes/miracles/libs/lang/zh-tw.php
+++ 000.old/blog.langnang/usr/themes/miracles/libs/lang/zh-tw.php
@@ -1,13 +1,13 @@
 <?php
 // 語言包信息
 $langInfo = array(
-    "name" => "簡體中文",
+    "name" => "簡體中文",
     "author" => "Eltrac",
     "last_update" => "2020\/5\/3"
 );
 // 文章
 $postTexts = array(
-    "read_more" => "閱讀全文",
+    "read_more" => "閱讀全文",
     "sort_title" => "文章分類",
     "post_time" => "該文章發佈於 %s",
     "post_views" => "該文章被瀏覽 %s 次",
@@ -17,7 +17,7 @@ $postTexts = array(
 );
 // 評論表單
 $commentFormTexts = array(
-    "nameInput" => "稱呼 *",
+    "nameInput" => "稱呼 *",
     "emailInput" => "郵箱 *",
     "urlInput" => "http(s):\/\/",
     "submit" => "評論",
@@ -25,7 +25,7 @@ $commentFormTexts = array(
 );
 // 評論列表
 $commentListTexts = array(
-    "byAuthor" => "博主",
+    "byAuthor" => "博主",
     "commentNum" => "已有 %s 條評論",
     "commentNumNone" => "暫無評論",
     "commentHidden" => "博主已關閉本頁面的評論顯示，您評論後將不會看到您的評論內容",
@@ -34,7 +34,7 @@ $commentListTexts = array(
 );
 // 頁腳
 $footerTexts = array(
-    "startTime" => "記錄已延續了 %s",
+    "startTime" => "記錄已延續了 %s",
     "startTimeDay" => "%s 天",
     "startTimeHour" => "%s 小時",
     "startTimeMin" => "%s 分",
@@ -42,7 +42,7 @@ $footerTexts = array(
 );
 // 頁眉
 $headerTexts = array(
-    "loginAlready" => "你已經登陸了",
+    "loginAlready" => "你已經登陸了",
     "loginAlreadyInfo" => "不過康納很樂意再次見到 %s 呢～",
     "loginAdminEntrance" => "進入後台",
     "loginTitle" => "登錄後台",
@@ -52,20 +52,20 @@ $headerTexts = array(
 );
 // 歸檔頁面
 $archivePageTexts = array(
-    "archiveTagTitle" => "文章標籤",
+    "archiveTagTitle" => "文章標籤",
     "archivePostTitle" => "文章歸檔",
     "archiveTimeYear" => "%s 年"
 );
 // 關於頁面
 $aboutPageTexts = array(
-    "aboutShareGithub" => "GitHub",
+    "aboutShareGithub" => "GitHub",
     "aboutShareQQ" => "QQ",
     "aboutShareB" => "Bilibili",
     "aboutShareEmail" => "E-mail"
 );
 // 說說頁面
 $sayingPageTexts = array(
-    "sayingPlaceholder" => "Anything interesting happening?",
+    "sayingPlaceholder" => "Anything interesting happening?",
     "sayingSubmit" => "發表新鮮事"
 );
 ?>
